257d58b9b3f4debed96f1b6ff8c8ebb515338391
Carson Burke <carsonburke22@gmail.com>
2021-07-08 00:34:21 -0700

    added escape key to close, foundation for panning function

--- ezLightbox.js
+++ ezLightbox.js
@@ -53,7 +53,7 @@ function addStyles() {
         user-select: none;
         transition: all 0.3s;
         animation: floatIn 0.4s;
-        aspect-ratio: 1/1;
+        overflow: hidden;
     }
     .closeButton {
 
@@ -157,25 +157,27 @@ window.onclick = function(click) {
 
     } else if (!element.classList.contains("lightboxImage")) {
 
-        // Hide lightbox when user clicks off
-
-        let lightboxContainers = document.getElementsByClassName("lightboxContainer")
-
-        for (let element of lightboxContainers) {
-
-            element.classList.add("lightboxContainerHide")
-        }
-
-        let closeButtons = document.getElementsByClassName("closeButton")
-
-        for (let element of closeButtons) {
-
-            element.classList.add("closeButtonHide")
-        }
+        closeLightbox()
     }
 }
 
+window.onkeydown = function(e) {
+
+    if (e.key == "Escape") {
+
+        closeLightbox()
+    }
+}
+
+
+// Hide lightbox when user scrolls
+
 window.onscroll = function() {
+
+    closeLightbox()
+}
+
+function closeLightbox() {
 
     let lightboxContainers = document.getElementsByClassName("lightboxContainer")
 
@@ -191,3 +193,136 @@ window.onscroll = function() {
         element.classList.add("closeButtonHide")
     }
 }
+
+(function() {
+    "use strict";
+
+    /* prevent multiple script inits */
+    if (typeof(window.lc_mouseDrag) == 'function') {
+        return true;
+    }
+
+
+    /* 
+     * Public class initializing the plugin for each targeted element 
+     * suggested ratio = 0.3
+     */
+    window.lc_mouseDrag = function(attachTo, ratio = 0.3, ignoreX = false, ignoreY = false) {
+        if (!attachTo) {
+            return console.error('You must provide a valid selector or DOM object as first argument');
+        }
+
+
+        /* get elements to attach event to */
+        const get_elems = function(selector) {
+            if (typeof(selector) != 'string') {
+                return (selector instanceof Element) ? [selector] : Object.values(selector);
+            }
+
+            // clean problematic selectors
+            (selector.match(/(#[0-9][^\s:,]*)/g) || []).forEach(function(n) {
+                selector = selector.replace(n, '[id="' + n.replace("#", "") + '"]');
+            });
+
+            return document.querySelectorAll(selector);
+        };
+
+
+        /* perform */
+        const mouseDrag = function($elem) {
+            let trackX = (!ignoreX) ? true : false,
+                trackY = (!ignoreY) ? true : false,
+
+                curDown = false,
+                curYPos = 0,
+                curXPos = 0,
+
+                startScrollY = 0,
+                startScrollX = 0,
+                scrollDif = 0,
+                animation = null;
+
+
+            $elem.addEventListener('mousedown', (e) => {
+                e.preventDefault();
+                curDown = true;
+
+                startScrollY = parseInt($elem.scrollTop, 10);
+                startScrollX = parseInt($elem.scrollLeft, 10);
+                curYPos = e.clientY;
+                curXPos = e.clientX;
+            });
+
+
+            $elem.addEventListener('mouseup', (e) => {
+                if (!ratio) {
+                    return true;
+                }
+
+                // smooth scroll
+                let currScrollY = $elem.scrollTop,
+                    scrollDiffY = (startScrollY - currScrollY) * -1,
+                    newScrollY = currScrollY + (scrollDiffY * ratio),
+
+                    currScrollX = $elem.scrollLeft,
+                    scrollDiffX = (startScrollX - currScrollX) * -1,
+                    newScrollX = currScrollX + (scrollDiffX * ratio);
+
+                let scroll_obj = {
+                    behavior: 'smooth'
+                };
+                if (trackY) {
+                    scroll_obj.top = newScrollY;
+                }
+                if (trackX) {
+                    scroll_obj.left = newScrollX;
+                }
+
+                animation = $elem.scroll(scroll_obj);
+            });
+
+
+
+            document.body.addEventListener('mouseup', (e) => {
+                curDown = false;
+            });
+
+
+
+            $elem.addEventListener('mousemove', (e) => {
+                if (curDown === true) {
+                    if (animation) {
+                        animation.pause();
+                    }
+
+                    let scroll_obj = {
+                        behavior: 'auto'
+                    };
+                    if (trackY) {
+                        scroll_obj.top = startScrollY + (curYPos - e.clientY);
+                    }
+                    if (trackX) {
+                        scroll_obj.left = startScrollX + (curXPos - e.clientX);
+                    }
+
+                    $elem.scroll(scroll_obj);
+                }
+            });
+        };
+
+
+        // init
+        get_elems(attachTo).forEach(($el) => {
+
+            // not for touch mobile devices
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                document.body.classList.add('lc_mousedrag_is_mobile');
+                return true;
+            }
+
+            mouseDrag($el);
+        });
+    };
+})();
+
+lc_mouseDrag('#lightboxImage')
